acc7cda32453a491387b44057550342c7b77c0d2
daniel portnoy <danlpornoty@gmail.com>
2013-12-02 21:25:17 +0200

    RED : added OneNum test func. which dosent pass the test

--- String Calculator TDD Kata 1/src/String_Calc_test.java
+++ String Calculator TDD Kata 1/src/String_Calc_test.java
@@ -9,16 +9,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import org.junit.Before;
+
 
 public class String_Calc_test {
 	
-	String_Calc calc = new String_Calc();
+	
+	 private String_Calc calc;
+
+     @Before
+     public void init()
+     {
+             calc = new String_Calc();
+     }
 	
 	@Test
 	public void EmptyStrRetZero()
 	{
-		String_Calc calc = new String_Calc();
 		assertEquals(calc.Add(""), 0);
+	}
+	
+	@Test
+	public void OneNum()
+	{
+		assertEquals(calc.Add("7"), 7);
 	}
 
 }
