40e7951feca8b27249b9599bfb7dd93bf6a9f151
daniel portnoy <danlpornoty@gmail.com>
2013-12-02 21:17:23 +0200

    RED : added EmptyStrRetZero  test func. which dosent pass the test

---
+++ String Calculator TDD Kata 1/src/String_Calc.java
@@ -0,0 +1,15 @@
+/*
+ * name : Daniel portnoy
+ * id : 307889287	
+ * gitHub login: danielpornoty
+ * 
+ * */
+
+public class String_Calc {
+
+	int Add(String numbers)
+	{
+		return -1;
+	}
+
+}
--- String Calculator TDD Kata 1/src/String_Calc_test.java
+++ String Calculator TDD Kata 1/src/String_Calc_test.java
@@ -1,4 +1,24 @@
+/*
+ * name : Daniel portnoy
+ * id : 307889287	
+ * gitHub login: danielpornoty
+ * 
+ * */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class String_Calc_test {
+	
+	String_Calc calc = new String_Calc();
+	
+	@Test
+	public void EmptyStrRetZero()
+	{
+		String_Calc calc = new String_Calc();
+		assertEquals(calc.Add(""), 0);
+	}
 
 }
