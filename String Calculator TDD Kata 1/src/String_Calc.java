5775d577414d4983bf930d107608c89d6489b06d
daniel portnoy <danlpornoty@gmail.com>
2013-12-02 21:37:24 +0200

    REFACTOR : added methods isEmpty and StringToInt at String_Calc

--- String Calculator TDD Kata 1/src/String_Calc.java
+++ String Calculator TDD Kata 1/src/String_Calc.java
@@ -7,11 +7,21 @@
 
 public class String_Calc {
 
-	int Add(String numbers)
+	int Add(String input)
 	{
-		if(numbers.isEmpty())
+		if(isEmpty(input))
 			return 0;
-		return 7;
+		return StringToInt(input);
 	}
 
+	private boolean isEmpty(String input)
+	{
+		return input.isEmpty();
+	}
+	
+	private int StringToInt(String input)
+	{
+		return Integer.parseInt(input);
+	}
+	
 }
